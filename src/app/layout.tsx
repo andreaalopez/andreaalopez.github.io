ab5348755bdf1e8eaf6fc92e147978b660201461
Andrea Lopez <apl2171@barnard.edu>
2025-01-17 23:24:55 -0500

    change title and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Andrea López</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${inter.className} antialiased dark:bg-slate-900 dark:text-white`}>{children}</body>
     </html>
   );
